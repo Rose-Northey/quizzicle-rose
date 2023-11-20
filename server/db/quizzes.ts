cc756c0facc8cca2655c7b08bc3a613e24c0b709
student <student@pop-os.localdomain>
2023-11-21 11:48:45 +1300

    create addNewQuiz database function

--- server/db/quizzes.ts
+++ server/db/quizzes.ts
@@ -4,3 +4,14 @@ import { Quiz, QuizSnakeCase, QuizData } from '../../models/quiz.ts'
 export async function getQuizzes(): Promise<Quiz[]> {
   return [] as Quiz[]
 }
+
+
+
+export async function addNewQuiz(quizData: QuizData):Promise<Quiz>{
+  await db('quizzes').insert(
+    {"quiz_name": quizData.quizName,
+    "last_updated": new Date(),
+    "is_public": quizData.isPublic,
+    "author_id": quizData.authorId}
+  )
+}
