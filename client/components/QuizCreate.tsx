7e45e194c3ba762ee0ec231a953b227c75acaa00
student <student@pop-os.localdomain>
2023-11-21 16:28:54 +1300

    create form for quiz component

--- client/components/QuizCreate.tsx
+++ client/components/QuizCreate.tsx
@@ -1,19 +1,37 @@
 import {useState} from 'react'
 
 function QuizCreate() {
-  const[isPublic, setIsPublic] = useState(false)
+  const[isPublic, setIsPublic] = useState<boolean>(false)
+  const[quizName, setQuizName] = useState<string>('')
 
   const handleSubmit = (event: React.FormEvent<HTMLInputElement>)=>{
     event.preventDefault()
-    
   }
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuizName(event.target.value);
+  };
+
+  const handleCheckbox = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setIsPublic(event.target.checked);
+  };
+
 
 
 
   return (<>
     <form onSubmit = {handleSubmit}>
-      <label>Quiz Name</label>
-      
+
+
+      <label>Quiz name
+        <input type='text' name={quizName} id={quizName} onChange={handleNameChange}/>
+      </label>
+
+      <label>
+          <input type='checkbox' checked={isPublic} onChange={handleCheckbox}/>
+      </label>
+      <button>Create and add questions</button>
+
 
     </form>
   </>)
