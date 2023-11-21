af9de17e48ce4206c0ff4720628763cbc1c96987
Murphy-Ardern <95775536+Murphy-Ardern@users.noreply.github.com>
2023-11-22 11:11:39 +1300

    Update QuizResult.tsx

--- client/components/QuizResult.tsx
+++ client/components/QuizResult.tsx
@@ -1,12 +1,5 @@
-
-
 function QuizResult() {
   return <>This component will show the results of your quiz</>
 }
 
 export default QuizResult
-//get correct_answer
-// if correct_answer = true then add 1 point to a points object
-// map through potential entire array of answers and add up points in object
-// use math prob division to work out a percentage. divide correct answer by no. of answers?
-// display these results on the page for the user to look at.
