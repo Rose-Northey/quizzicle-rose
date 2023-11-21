7e342bb21b673c05de2a3b5f747825af7f1a14f8
Murphy Ardern <smurfyman86@gmail.com>
2023-11-22 12:53:30 +1300

    added initial setup for scoring

--- client/components/QuizResult.tsx
+++ client/components/QuizResult.tsx
@@ -3,3 +3,8 @@ function QuizResult() {
 }
 
 export default QuizResult
+//get correct_answer
+// if correct_answer = true then add 1 point to a points object
+// map through potential entire array of answers and add up points in object
+// use math prob division to work out a percentage. divide correct answer by no. of answers?
+// display these results on the page for the user to look at.
