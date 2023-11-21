14904a56e99e542bcbf990e70d0022addc69ad53
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-22 11:37:35 +1300

    add Field is required warning

--- client/components/QuizCreate.tsx
+++ client/components/QuizCreate.tsx
@@ -1,47 +1,60 @@
-import {useState} from 'react'
-import {AddQuiz} from '../api'
-import {useNavigate} from 'react-router-dom'
-
+import { useState } from 'react'
+import { AddQuiz } from '../api'
+import { useNavigate } from 'react-router-dom'
 
 function QuizCreate() {
-  const[isPublic, setIsPublic] = useState<boolean>(false)
-  const[quizName, setQuizName] = useState<string>('')
+  const [isPublic, setIsPublic] = useState<boolean>(false)
+  const [quizName, setQuizName] = useState<string>('')
+
   const navigate = useNavigate()
+  //when quizName is "", there is a p element under the form which is set to no class, when the quiz name is not "", it disappears by setting the class name to "hidden"
 
-
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=>{
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const newQuizId = await AddQuiz({quizName, isPublic})
+    const newQuizId = await AddQuiz({ quizName, isPublic })
     navigate(`/${newQuizId}/add-question`)
   }
 
   const handleNameChange = (event: React.ChangeEvent<HTMLFormElement>) => {
-    setQuizName(event.target.value);
-  };
+    setQuizName(event.target.value)
+  }
 
   const handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsPublic(event.target.checked);
-  };
+    setIsPublic(event.target.checked)
+  }
+
+  return (
+    <>
+      <div className="create">
+        <form className="vflex" onSubmit={handleSubmit}>
+          <label className="vflex">
+            Quiz name
+            <input
+              className="nameInput"
+              type="text"
+              name={quizName}
+              id={quizName}
+              onChange={handleNameChange}
+              required
+            />
+            <p className={quizName === ''?'error':'error hidden'} >Field is required</p>
+          </label>
 
 
+          <label>
+            <input
+              type="checkbox"
+              checked={isPublic}
+              onChange={handleCheckbox}
+            />
+            Should this quiz be public to all users?
+          </label>
 
-
-  return (<>
-  <div className='create'>
-    <form className = 'vflex' onSubmit = {handleSubmit}>
-      <label className='vflex'>Quiz name
-        <input className='nameInput'type='text' name={quizName} id={quizName} onChange={handleNameChange}/>
-      </label>
-
-      <label> 
-          <input type='checkbox' checked={isPublic} onChange={handleCheckbox}/>Should this quiz be public to all users?
-      </label>
-
-      <button>Create and add questions</button>
-    </form>
-  </div>
-  </>)
+          <button>Create and add questions</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default QuizCreate
