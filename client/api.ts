024b95dbb88b9b9ff57eab64ae4cf05cc0760778
student <student@pop-os.localdomain>
2023-11-21 16:47:49 +1300

    add api function

--- client/api.ts
+++ client/api.ts
@@ -35,3 +35,9 @@ export async function getQuizzes(): Promise<Quiz[]> {
 function logError(err: Error) {
   console.error('Error consuming the API (in client/api.js):', err.message)
 }
+
+export async function AddQuiz({quizName, isPublic}:{quizName:string, isPublic: boolean}){
+  const httpRequestObject = await request.post(`${rootUrl}/quizzes`).send({quizName, isPublic})
+  const newQuizId =  httpRequestObject.body
+  return newQuizId
+}
