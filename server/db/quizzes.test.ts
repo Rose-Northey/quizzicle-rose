c8fee72695d6771f99d77307e3daf2db22124797
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-22 14:48:32 +1300

    add db test

--- server/db/quizzes.test.ts
+++ server/db/quizzes.test.ts
@@ -9,12 +9,6 @@ beforeEach(() => {
   return connection.seed.run()
 })
 
-
-test('testing the testing', () => {
-  it('returns positive is truth is truthy')
-  expect(true).toBeTruthy()
-})
-
 describe( 'addNewQuiz', ()=> {
   const quizData = {
     quizName: 'My First Quiz',
@@ -24,6 +18,6 @@ describe( 'addNewQuiz', ()=> {
 
   it('adds a new quiz to the quizzes database', async()=>{
     const newQuizId = await addNewQuiz(quizData)
-    expectTypeOf(newQuizId).toEqualTypeOf(1)
+    expect(newQuizId).toEqual([{"quiz_id":4}])
   })
 })
