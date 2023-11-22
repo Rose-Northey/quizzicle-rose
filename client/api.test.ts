9f3e4d83ff0b7d9e3b8e3240b6575da9eea9025e
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-22 15:57:59 +1300

    add expected result of the server side db test as 4

--- client/api.test.ts
+++ client/api.test.ts
@@ -1,6 +1,16 @@
-import { expect, it, test } from 'vitest'
+import { expect, it, test, describe, vi } from 'vitest'
+import supertest from 'supertest'
+import {AddQuiz} from './api'
 
 test('testing the testing', () => {
   it('returns positive is truth is truthy')
   expect(true).toBeTruthy()
 })
+
+describe('test clientside api posts 4th row', ()=>{
+
+  it('retuns the number 4 after posting the 4th row', async()=>{
+    const newQuizId = await AddQuiz({quizName: 'testQuiz', isPublic: true})
+  })
+} 
+)
--- server/routes/quizzes.test.ts
+++ server/routes/quizzes.test.ts
@@ -1,7 +1,7 @@
 import { expect, it, test, vi, describe } from 'vitest'
 import request from 'supertest'
 import server from '../server'
-import { JSDOM } from 'jsdom'
+// import { JSDOM } from 'jsdom'
 import * as db from '../db/quizzes'
 
 vi.mock('../db/quizzes')
@@ -12,19 +12,15 @@ test('testing the testing', () => {
 })
 
 describe('post of a new quiz name and isPublic', ()=>{
-  it('posts data and returns the 4th id number', async ()=>{
-    const newQuizEntry = { 
-      quizName: 'testingQuiz',
-      lastUpdated: new Date(),
-      isPublic: false
-    }
-    
+  it('posts data and returns the 4th id number', async ()=>{    
+
     vi.mocked(db.addNewQuiz).mockImplementation(async()=>{
       return [{quiz_id: 4}];
     })
 
     const res = await request(server).post('/api/v1/quizzes')
     expect(res.statusCode).toBe(200)
+    expect(res.body).toBe(4)
   
   })
 
