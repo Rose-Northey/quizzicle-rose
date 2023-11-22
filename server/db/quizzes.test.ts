4181f5bae4d2c614bae889f472d16a06bbd660bc
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-22 14:35:30 +1300

    add seeds and migrations to test

--- server/db/quizzes.test.ts
+++ server/db/quizzes.test.ts
@@ -1,6 +1,29 @@
-import { expect, it, test } from 'vitest'
+import { expect, it, test, describe, expectTypeOf, beforeAll, beforeEach } from 'vitest'
+import {addNewQuiz} from './quizzes'
+import connection from './connection'
+
+beforeAll(() => {
+  return connection.migrate.latest()
+})
+beforeEach(() => {
+  return connection.seed.run()
+})
+
 
 test('testing the testing', () => {
   it('returns positive is truth is truthy')
   expect(true).toBeTruthy()
+})
+
+describe( 'addNewQuiz', ()=> {
+  const quizData = {
+    quizName: 'My First Quiz',
+    lastUpdated: new Date(),
+    isPublic: false
+  }
+
+  it('adds a new quiz to the quizzes database', async()=>{
+    const newQuizId = await addNewQuiz(quizData)
+    expectTypeOf(newQuizId).toEqualTypeOf(1)
+  })
 })
