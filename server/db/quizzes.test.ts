7c4ee0d69105e505f8644b458c7c50e3901abca9
student <student@pop-os.localdomain>
2023-11-23 14:04:45 +1300

    form nearly complete

--- server/db/quizzes.test.ts
+++ server/db/quizzes.test.ts
@@ -4,3 +4,8 @@ test('testing the testing', () => {
   it('returns positive is truth is truthy')
   expect(true).toBeTruthy()
 })
+
+test('testing database returns correct key format'), () => {
+  it('returns data in camelcase format')
+  expect
+}
