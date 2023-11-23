f7f727371771e763e576e15380020c58e7ae5f17
Elise Wei <elise.wei@gmail.com>
2023-11-24 06:55:57 +1300

    Base changes

--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { routes } from './routes.tsx'
 
 const queryClient = new QueryClient()
@@ -24,6 +25,7 @@ root.render(
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </Auth0Provider>
 )
