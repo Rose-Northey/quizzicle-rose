4dbf650acbd2d5ca2f0c0f106836423edc824788
Arianne Hallgarth <alvarezarianne15@yahoo.com>
2023-11-23 16:36:55 +1300

    Testing

--- server/db/quizzes.test.ts
+++ server/db/quizzes.test.ts
@@ -8,7 +8,7 @@ import {
   beforeEach,
   afterAll,
 } from 'vitest'
-import { addNewQuiz } from './quizzes'
+import { addNewQuiz, getQuizzes } from './quizzes'
 import connection from './connection'
 
 beforeAll(() => {
@@ -18,18 +18,39 @@ beforeEach(() => {
   return connection.seed.run()
 })
 
-describe('addNewQuiz', () => {
-  const quizData = {
-    quizName: 'My First Quiz',
-    lastUpdated: new Date(),
-    isPublic: false,
-  }
-
-  it('adds a new row to the quizzes database', async () => {
-    const newQuizId = await addNewQuiz(quizData)
-    expect(newQuizId).toEqual([{ quiz_id: 4 }])
+describe('getQuizzes', () => {
+  const quizObjectArray = [
+    {
+      quizId: 1,
+      quizName: 'My First Quiz',
+    },
+    {
+      quizId: 2,
+      quizName: 'My Second Quiz',
+    },
+    {
+      quizId: 3,
+      quizName: 'My Fancy Quiz',
+    },
+  ]
+  it('displays list of quizzes', async () => {
+    const quizList = await getQuizzes()
+    expect(quizList).toEqual(quizObjectArray)
   })
 })
+
+// describe('addNewQuiz', () => {
+//   const quizData = {
+//     quizName: 'My First Quiz',
+//     lastUpdated: new Date(),
+//     isPublic: false,
+//   }
+
+//   it('adds a new row to the quizzes database', async () => {
+//     const newQuizId = await addNewQuiz(quizData)
+//     expect(newQuizId).toEqual([{ quiz_id: 4 }])
+//   })
+// })
 
 afterAll(() => {
   return connection.destroy()
--- server/db/quizzes.ts
+++ server/db/quizzes.ts
@@ -2,7 +2,6 @@ import db from './connection.ts'
 import { Quiz, QuizSnakeCase, QuizData } from '../../models/quiz.ts'
 
 export async function getQuizzes(): Promise<Quiz[]> {
-  console.log('whatever')
   return await db('quizzes').select(
     'quiz_id as quizId',
     'quiz_name as quizName'
