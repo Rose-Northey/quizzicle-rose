a2b5b89c4482eb95bf89e53c01aecac04c6235af
Murphy Ardern <smurfyman86@gmail.com>
2023-11-23 15:24:36 +1300

    setup useQuery for score analysis

--- client/components/QuizResult.tsx
+++ client/components/QuizResult.tsx
@@ -1,26 +1,30 @@
+//import thingy
+import { useQuery } from "@tanstack/react-query"
+import server from "../../server/server"
+
 function QuizResult() {
- 
+  
+  //make useQuery
+  const getScore = async () => {
+    const data = await server.get('api/:quizId/my-result')
+    return data
+  }
+  let score = 0
+//https://dev.to/abeinevincent/data-fetching-from-an-api-using-react-query-usequery-hook-explained-in-plain-english-4eei
+  
 const correctCheck = arr.map(id => {
   id.answer
-  let score = [0]
     if (id.answer === correct){
       score++
     }
-  else 
-  })
- 
- 
-///all variables below this point are filler only. Do not use
+  }
+  )
+  //all variables below this point are filler only. Do not use
  return (
   <div>
     <h1>{`${quizId}`}</h1>
-    <p>You answered {`${correct}`} out of {`${totalQuestions}`} questions correctly and scored {`${calcAmount}`}%</p>
+    <p>You answered {`${score}`} out of {`${totalQuestions}`} questions correctly and scored {`${calcAmount}`}%</p>
   </div>
 )}
 
 export default QuizResult
-//get correct_answer
-// if correct_answer = true then add 1 point to a points object
-// map through potential entire array of answers and add up points in object
-// use math prob division to work out a percentage. divide correct answer by no. of answers?
-// display these results on the page for the user to look at.
