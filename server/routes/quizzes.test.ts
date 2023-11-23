5824aec1d66d735d50f970d2d543f4274db4e6d0
student <student@pop-os.localdomain>
2023-11-24 09:48:27 +1300

    move the test code into questions.test.ts file

--- server/routes/quizzes.test.ts
+++ server/routes/quizzes.test.ts
@@ -10,23 +10,3 @@ test('testing the testing', () => {
   expect(true).toBeTruthy()
 })
 
-vi.mock('../db/db')
-describe('POST /api/vi/:quizId/add-question',()=>{
-  it('it returns status code 200 if question is successfully added', async ()=>{
-    //Arrange
-   
-    const mockNewQuestion={
-      question_text:"test question",
-      correct_answer: "test correct answer",
-      incorrect_answer1: "test incorrect answer1",
-      incorrect_answer2: "test incorrect answer2",
-      incorrect_answer3: "test incorrect answer3"
-    }
-  vi.mocked(db.insertQuestion).mockResolvedValue(mockNewQuestion as unknown as unknown[])
-    //Act
-  const response = await request(server).post('/api/v1/questions/1/add-question').send(mockNewQuestion)
-    //Assert
-  expect(response.status).toBe(200)
-  expect(response.body.question_text).toBe("test question")
-})
-})
