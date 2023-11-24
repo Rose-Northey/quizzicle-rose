87a7c95192a0fe3f65fc2e0881645e4b66eae6d2
Elise Wei <elise.wei@gmail.com>
2023-11-24 13:18:05 +1300

    Some merge conflict fixes

--- server/routes/quizzes.test.ts
+++ server/routes/quizzes.test.ts
@@ -1,6 +1,5 @@
 import { Quiz } from '../../models/quiz.ts'
-import { QuestionData, QuestionSnakeCase } from '../../models/question'
-import { describe, expect, it, test, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import server from '../server.ts'
 import request from 'supertest'
 import * as db from '../db/quizzes'
@@ -9,26 +8,27 @@ vi.mock('../db/quizzes')
 
 describe('get of a quiz and all of its questions, questions pushed into an answers array', () => {
   it('gets data and checks if an answers array', async () => {
-
-    vi.mocked(db.getSingleQuizQuestions).mockImplementation(async()=>{
+    vi.mocked(db.getSingleQuizQuestions).mockImplementation(async () => {
       return [
         {
-          "quizName": "My First Quiz",
-          "quizId": 1,
-          "questionId": 1,
-          "questionText": "Pretend this is a longer question?",
-          "answers": [
-            "correct answer",
-            "incorrect answer1",
-            "incorrect answer2",
-            "incorrect answer3"
-        ]}];
+          quizName: 'My First Quiz',
+          quizId: 1,
+          questionId: 1,
+          questionText: 'Pretend this is a longer question?',
+          answers: [
+            'correct answer',
+            'incorrect answer1',
+            'incorrect answer2',
+            'incorrect answer3',
+          ],
+        },
+      ]
     })
-    const res = await request(server).get('/api/v1/1')
+    const res = await request(server).get('/api/v1/quizzes/1')
     expect(res.statusCode).toBe(200)
-  )}
-)}
-         
+  })
+})
+
 describe('GET /api/v1/quizzes', () => {
   it('GETs all quizzes', async () => {
     // ARRANGE
@@ -65,25 +65,23 @@ describe('GET /api/v1/quizzes', () => {
   })
 })
 
-describe('post of a new quiz name and isPublic', ()=>{
-  it('posts data and returns the 4th id number', async ()=>{    
-
-    vi.mocked(db.addNewQuiz).mockImplementation(async()=>{
-      return [{quiz_id: 4}];
+describe('post of a new quiz name and isPublic', () => {
+  it('posts data and returns the 4th id number', async () => {
+    vi.mocked(db.addNewQuiz).mockImplementation(async () => {
+      return [{ quiz_id: 4 }]
     })
 
     const res = await request(server).post('/api/v1/quizzes')
     expect(res.statusCode).toBe(200)
     expect(res.body).toBe(4)
-  
   })
-  it('throws an error if the database fails'), async()=>{
-    vi.mocked(db.addNewQuiz).mockImplementation(async()=>{
-      throw new Error('Database error');
-    })
-    
-    const res = await request(server).post('/api/v1/quizzes')
-    expect(res.statusCode).toBe(500);
-    
-  }
+  it('throws an error if the database fails'),
+    async () => {
+      vi.mocked(db.addNewQuiz).mockImplementation(async () => {
+        throw new Error('Database error')
+      })
+
+      const res = await request(server).post('/api/v1/quizzes')
+      expect(res.statusCode).toBe(500)
+    }
 })
